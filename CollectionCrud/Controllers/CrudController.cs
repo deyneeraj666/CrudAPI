fe6657aa3d4ded35376d8fcb181b40deb56c79f3
Niroj <nirojdey@outlook.com>
2022-02-15 13:36:49 +0530

    initial Commit

---
+++ CollectionCrud/Controllers/CrudController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        static List<string> crudlst = new List<string>() { "Roffel","Ram"};
+
+        [HttpGet]
+        public List<string> getList()
+        {
+            return crudlst;
+        }
+        [HttpPost]
+        public void postList(string value)
+        {
+            crudlst.Add(value);
+        }
+        [HttpPut]
+        public void putList(int i,string value)
+        {
+            crudlst[i]=value;
+        }
+        [HttpDelete]
+        public void deleteList(int i)
+        {
+            crudlst.RemoveAt(i);
+        }
+    }
+}
